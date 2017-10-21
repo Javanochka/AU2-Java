756654447f5305c3474d54bafc2e1db50a550ed2
Anna Nikiforovskaya <nikiannanik@gmail.com>
2017-10-21 10:39:06 +0300

    hw4: added throws-javadocs, notnull, changed zip file checking

--- HW4_ZipFile/src/main/java/ru/spbau/nikiforovskaya/util/Unzipper.java
+++ HW4_ZipFile/src/main/java/ru/spbau/nikiforovskaya/util/Unzipper.java
@@ -1,7 +1,10 @@
 package ru.spbau.nikiforovskaya.util;
 
+import org.jetbrains.annotations.*;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -17,17 +20,32 @@ import java.util.zip.ZipFile;
  */
 public class Unzipper {
 
+
+    /**
+     * Checks if file a zip, by opening it and reading the first four bytes.
+     * @param file a file to check if zip
+     * @return {@code true} if file is a zip, {@code false} otherwise.
+     */
+    private static boolean isZip(@NotNull Path file) {
+        try (RandomAccessFile raf = new RandomAccessFile(file.toString(), "r")) {
+            long n = raf.readInt();
+            return n == 0x504B0304 || n == 0x504B0506 || n == 0x504B0708;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
     /**
      * Returns all zip files in the path you pass to the function.
      * @param path path where to search for zip files
      * @return list of zip files found
-     * @throws IOException
+     * @throws IOException if couldn't open the file on the path.
      */
-    public static ArrayList<File> getAllZipFiles(String path) throws IOException {
+    public static ArrayList<File> getAllZipFiles(@NotNull String path) throws IOException {
         ArrayList<File> result = new ArrayList<>();
         try (Stream<Path> stream = Files.walk(Paths.get(path))) {
             for (Path file : stream.collect(Collectors.toList())) {
-                if (file.getFileName().toString().endsWith(".zip")) {
+                if (isZip(file)) {
                     result.add(file.toFile());
                 }
             }
@@ -41,10 +59,11 @@ public class Unzipper {
      * @param regex a regular expression to be matched
      * @return list of ZipFiles, containing needed files.
      */
-    public static ArrayList<ZipEntry> getZipEntriesMatchingRegex(ZipFile source, String regex) {
+    public static ArrayList<ZipEntry> getZipEntriesMatchingRegex(@NotNull ZipFile source,
+                                                                 @NotNull String regex) {
         ArrayList<ZipEntry> result = new ArrayList<>();
-        for (Enumeration<? extends ZipEntry> fileEnum = source.entries();
-             fileEnum.hasMoreElements();) {
+        Enumeration<? extends ZipEntry> fileEnum = source.entries();
+        while (fileEnum.hasMoreElements()) {
             ZipEntry file = fileEnum.nextElement();
             if (!file.isDirectory() && file.getName().matches(regex)) {
                 result.add(file);
@@ -60,10 +79,11 @@ public class Unzipper {
      * @param file a zip entry to extract from zip file
      * @param zipFile a zip file to extract from
      * @param dirPath a path to extract to
-     * @throws IOException
+     * @throws IOException if couldn't extract from zip file,
+     * or couldn't write to destination.
      */
-    public static void extractZipEntry(ZipEntry file, File zipFile, String dirPath)
-            throws IOException {
+    public static void extractZipEntry(@NotNull ZipEntry file, @NotNull File zipFile,
+                                       @NotNull String dirPath) throws IOException {
         File outputFile = Paths.get(dirPath,
                 zipFile.getName().substring(0, zipFile.getName().length() - 4),
                 file.getName()).toFile();
@@ -81,10 +101,11 @@ public class Unzipper {
      * @param source path where search for zip files
      * @param regex a regular expression to match files in zip files
      * @param destination a path where to extract files to.
-     * @throws IOException
+     * @throws IOException if couldn't open the files on the source,
+     * or couldn't write to destination.
      */
-    public static void extractFilesByRegex(String source, String regex, String destination)
-            throws IOException {
+    public static void extractFilesByRegex(@NotNull String source, @NotNull String regex,
+                                           @NotNull String destination) throws IOException {
         ArrayList<File> allZipFiles = getAllZipFiles(source);
         for (File file : allZipFiles) {
             ArrayList<ZipEntry> entries = getZipEntriesMatchingRegex(new ZipFile(file), regex);
