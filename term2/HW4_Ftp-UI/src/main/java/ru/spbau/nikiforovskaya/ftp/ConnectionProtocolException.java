e0a8fce8ee13f925afa78a071c8d040754216543
Anna Nikiforovskaya <nikiannanik@gmail.com>
2018-05-30 19:40:50 +0300

    hw4-ftp-ui: deleted extra folder

--- term2/HW4_Ftp-UI/src/main/java/ru/spbau/nikiforovskaya/ftp/ConnectionProtocolException.java
+++
@@ -1,9 +0,0 @@
-package ru.spbau.nikiforovskaya.ftp;
-
-/** Exception, which is thrown when something happens not due to protocol. */
-public class ConnectionProtocolException extends Exception {
-    /** Creates a connection protocol exception */
-    public ConnectionProtocolException() {
-        super("The query does not match the protocol");
-    }
-}
